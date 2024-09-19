cba61662f3e87dfd89a5f44341accab29e2b1ff3
Mostary Jahan <mostaryjahan01@gmail.com>
2024-09-19 11:07:21 +0600

    fetched products

--- src/features/products/ProductListView.jsx
+++ src/features/products/ProductListView.jsx
@@ -1,20 +1,32 @@
 import { useEffect } from "react";
 import { fetchProducts } from "./productSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductListView = () => {
-
-    const dispatch = useDispatch()
+    const {products, error, isLoading } = useSelector(state => state.productsR);
+    // console.log(products);
+    const dispatch = useDispatch();
 
     useEffect(() =>{
        dispatch(fetchProducts());
-    }, []);
+    }, [dispatch]);
 
 
-
-    return (
+    return ( 
         <div>
-            List of products
+            {isLoading && <p>Loading products............</p>}
+            {error && <p>{error}</p>}
+            {!isLoading && !error && products && products.length>0  &&
+            products.map(product => {
+              
+                return  <article key={product.id}>
+                    <h3>{product.title}</h3>
+                    <p>{product.description}</p>
+                    <h5>{product.price}</h5>
+
+                </article>
+            })
+            }
         </div>
     );
 };
